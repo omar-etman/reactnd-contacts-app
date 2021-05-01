5f59dc0825672fa2ef4cfd4d9c88d394197c4b1d
omar-etman <omartareketman@gmail.com>
2021-05-01 06:20:02 +0200

    list contacts

---
+++ src/ListContacts.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ListContacts extends Component {
+
+    render(){
+        return (
+            <ol className='contact-list'>
+                
+            </ol>
+        )
+    }
+
+
+}
+
+export default ListContacts
